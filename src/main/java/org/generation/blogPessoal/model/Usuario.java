6704b020293e3aeea07a3ea24b8831e24c5ba45f
Cássia Lima <cassiadosreislima@gmail.com>
2021-10-14 21:06:42 -0300

    Deploy inicial- Blog pessoal

---
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -0,0 +1,115 @@
+package org.generation.blogPessoal.model;
+
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+import io.swagger.annotations.ApiModelProperty;
+
+@Entity
+@Table (name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 100)
+	private String nome;
+	
+	@ApiModelProperty(example = "email@email.com.br")
+	@NotNull
+	@Email(message = "O atributo Usuário deve ser um email válido!")
+	private String usuario;
+
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String senha;
+	
+	@Column(name = "dt_nascimento")
+	@JsonFormat(pattern="yyyy-MM-dd")
+	private Date dataNascimento;
+	
+	
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+
+	public Usuario(long id, @Size(min = 2, max = 100) String nome, @Size(min = 5, max = 100) String usuario,
+			@Size(min = 5, max = 100) String senha) {
+		
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+	
+	
+
+	public Usuario() {
+		
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	
+	
+}
